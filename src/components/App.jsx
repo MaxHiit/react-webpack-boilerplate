1adbff732b7887b750394d0dde9ddda0ddbba95b
MaxHiit <maxime.baronce@hotmail.fr>
2021-03-02 00:18:03 +0100

    feat(app): Places the layout and the different pages

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Layout from '../Layout/Layout';
+import Home from '../pages/Home';
+
+const App = () => {
+  <>
+    <Layout>
+      <Home />
+    </Layout>
+  </>;
+};
+
+export default App;
