bbccd00680f958e3fd87f85fe9d126dc9e81ab23
MaxHiit <maxime.baronce@hotmail.fr>
2021-03-02 00:53:34 +0100

    feat(app:component): add return into my component for render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import Layout from '../Layout/Layout';
 import Home from '../pages/Home';
 
 const App = () => {
-  <>
-    <Layout>
-      <Home />
-    </Layout>
-  </>;
+  return (
+    <>
+      <Layout>
+        <Home />
+      </Layout>
+    </>
+  );
 };
 
 export default App;
